63c53a158f9380d501c91a89802f3c8fac1ae668
Taslima Rumky <taslimarumy17@gmail.com>
2023-05-31 14:53:49 +0600

    final Commit

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,53 +1,135 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
 const Problem1 = () => {
+    const [show, setShow] = useState("all");
+    const [name, setName] = useState("");
+    const [status, setStatus] = useState("");
+    const [items, setitems] = useState([]);
 
-    const [show, setShow] = useState('all');
+    useEffect(() => {
+        const storeditems = localStorage.getItem("items");
+        if (storeditems) {
+            setitems(JSON.parse(storeditems));
+        }
+    }, []);
 
-    const handleClick = (val) =>{
+    useEffect(() => {
+        localStorage.setItem("items", JSON.stringify(items));
+    }, [items]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newTask = { name, status };
+        setitems([...items, newTask]);
+        setName("");
+        setStatus("");
+    };
+
+    const handleClick = (val) => {
         setShow(val);
-    }
+    };
+
+    const filtereditems = items.filter((task) => {
+        if (show === "active") {
+            return task.status === "Active";
+        } else if (show === "completed") {
+            return task.status === "Completed";
+        }
+        return true; // show "all" items
+    });
+
+    const sorteditems = filtereditems.sort((a, b) => {
+        if (a.status === "Active") {
+            return -1;
+        } else if (b.status === "Active") {
+            return 1;
+        } else if (a.status === "Completed" && b.status !== "Active") {
+            return -1;
+        } else if (b.status === "Completed" && a.status !== "Active") {
+            return 1;
+        }
+        return 0;
+    });
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form
+                        className="row gy-2 gx-3 align-items-center mb-4"
+                        onSubmit={handleSubmit}
+                    >
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Status"
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button
+                                className={`nav-link ${show === "all" && "active"}`}
+                                type="button"
+                                onClick={() => handleClick("all")}
+                            >
+                                All
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button
+                                className={`nav-link ${show === "active" && "active"}`}
+                                type="button"
+                                onClick={() => handleClick("active")}
+                            >
+                                Active
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button
+                                className={`nav-link ${show === "completed" && "active"}`}
+                                type="button"
+                                onClick={() => handleClick("completed")}
+                            >
+                                Completed
+                            </button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {sorteditems.map((task, index) => (
+                                <tr key={index}>
+                                    <td>{task.name}</td>
+                                    <td>{task.status}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,244 @@
-import React from 'react';
+
+
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
 const Problem2 = () => {
+    const [showModalA, setShowModalA] = useState(false);
+    const [showModalB, setShowModalB] = useState(false);
+    const [showModalC, setShowModalC] = useState(false);
+    const [onlyEven, setOnlyEven] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredContacts, setFilteredContacts] = useState([]);
+    const [contact, setContact] = useState([]);
+    const [selectedContact, setSelectedContact] = useState(null);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://contact.mediusware.com/api/contacts/');
+                const jsonData = await response.json();
+                setContact(jsonData.results);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleAllContactsClick = () => {
+        setShowModalA(true);
+        setShowModalB(false);
+        setShowModalC(false);
+    };
+
+    const handleUSContactsClick = () => {
+        setShowModalA(false);
+        setShowModalB(true);
+        setShowModalC(false);
+    };
+
+
+    const handleCloseModalClick = () => {
+        setShowModalA(false);
+        setShowModalB(false);
+        setShowModalC(false);
+    };
+    const handleContactsClick = async (contact) => {
+
+
+        setShowModalA(false);
+        setShowModalB(false);
+        setShowModalC(true);
+        setSelectedContact(contact);
+
+    };
+
+    const handleCheckboxChange = (event) => {
+        setOnlyEven(event.target.checked);
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        // Function to filter contacts based on search term and even/odd condition
+        const filterContacts = () => {
+            let filtered = contact;
+
+            if (searchTerm) {
+                filtered = filtered.filter((c) =>
+                    c.phone.includes(searchTerm.toLowerCase())
+                );
+            }
+
+            if (onlyEven) {
+                filtered = filtered.filter((c) => c.id % 2 === 0);
+            }
+
+            setFilteredContacts(filtered);
+        };
+
+        // Delayed filtering on typing
+        const delayTimer = setTimeout(filterContacts, 300);
+
+        return () => clearTimeout(delayTimer);
+    }, [searchTerm, onlyEven]);
+
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+                <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button
+                        className="btn btn-lg btn-outline-primary"
+                        style={{ color: "#46139f" }}
+                        type="button"
+                        onClick={handleAllContactsClick}
+                    >
+                        All Contacts
+                    </button>
+                    <button
+                        className="btn btn-lg btn-outline-warning"
+                        style={{ color: "#ff7f50" }}
+                        type="button"
+                        onClick={handleUSContactsClick}
+                    >
+                        US Contacts
+                    </button>
                 </div>
-                
+                {/* modal A */}
+                <Modal show={showModalA} onHide={handleCloseModalClick}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal A</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Search..."
+                                value={searchTerm}
+                                onChange={handleSearchInputChange}
+                            />
+                        </div>
+                        {filteredContacts.map((c) => (
+                            <p key={c.id} onClick={() => handleContactsClick(c)} >{c.phone}</p>
+                        ))}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            variant="primary"
+                            style={{ color: "#46139f" }}
+                            onClick={handleAllContactsClick}
+                        >
+                            All Contact
+                        </Button>
+                        <Button
+                            variant="primary"
+                            style={{ color: "#ff7f50" }}
+                            onClick={handleUSContactsClick}
+                        >
+                            US Contact
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            style={{ backgroundColor: "#46139f", borderColor: "#46139f" }}
+                            onClick={handleCloseModalClick}
+                        >
+                            Close
+                        </Button>
+                        <div className="form-check">
+                            <Form.Check
+                                id="checkboxA"
+                                label="Only even"
+                                checked={onlyEven}
+                                onChange={handleCheckboxChange}
+                            />
+                        </div>
+                    </Modal.Footer>
+                </Modal>
+
+                {/* modal B */}
+                <Modal show={showModalB} onHide={handleCloseModalClick}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal B</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Search..."
+                                value={searchTerm}
+                                onChange={handleSearchInputChange}
+                            />
+                        </div>
+                        {filteredContacts
+                            .filter((c) => c.country.name === "United States")
+                            .map((c) => (
+                                <p key={c.id} onClick={() => handleContactsClick(c)} >{c.phone}</p>
+                            ))}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            variant="primary"
+                            style={{ color: "#46139f" }}
+                            onClick={handleAllContactsClick}
+                        >
+                            All Contact
+                        </Button>
+                        <Button
+                            variant="primary"
+                            style={{ color: "#ff7f50" }}
+                            onClick={handleUSContactsClick}
+                        >
+                            US Contact
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            style={{ backgroundColor: "#46139f", borderColor: "#46139f" }}
+                            onClick={handleCloseModalClick}
+                        >
+                            Close
+                        </Button>
+                        <div className="form-check">
+                            <Form.Check
+                                id="checkboxB"
+                                label="Only even"
+                                checked={onlyEven}
+                                onChange={handleCheckboxChange}
+                            />
+                        </div>
+                    </Modal.Footer>
+                </Modal>
+                {/* Modal C */}
+                {selectedContact && (
+                    <Modal show={showModalC} onHide={handleCloseModalClick}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal C</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>Phone: {selectedContact.phone}</p>
+                            <p>Country: {selectedContact.country.name}</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+
+                            <Button
+                                variant="secondary"
+                                style={{ backgroundColor: "#46139f", borderColor: "#46139f" }}
+                                onClick={handleCloseModalClick}
+                            >
+                                Close
+                            </Button>
+
+                        </Modal.Footer>
+                    </Modal>
+                )}
             </div>
         </div>
     );
